7f81074298ac5a9b2be1dc03e8e2118a15f5487d
Yangxinsix <yangxindsg@163.com>
2022-07-07 14:16:48 +0200

    minor revision

---
+++ PaiNN/__init__.py
@@ -0,0 +1,3 @@
+from PaiNN import model
+from PaiNN import data
+from PaiNN import calculator
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PaiNN",
+    version="0.0.1",
+    description="Library for implementation of message passing neural networks in Pytorch",
+    author="xinyang",
+    author_email="xinyang@dtu.dk",
+    url = "https://github.com/Yangxinsix/PaiNN-model",
+    packages=["PaiNN"],
+)
